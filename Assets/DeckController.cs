ee0f895de50e3fbd1022c610c6e2eb14afdac759
jackblack0094 <syomzn10@gmail.com>
2021-05-14 17:09:17 +0900

    DeckControllerにコメントを作成

--- Assets/DeckController.cs
+++ Assets/DeckController.cs
@@ -13,6 +13,9 @@ public class DeckController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ///デッキにカードCardControllerクラスを継承したオブジェクトが置かれたとき(？)
+        ///デッキに2枚以上カードがあればそれをZ軸で積み上げる
+        ///1枚目のカードを置いた時には何も起こらない
+        ///2枚目以降のカードを置いたときに1枚目のカードの座標を取ってきて1枚目の座標のZ+1にカードを動かす
     }
 }
