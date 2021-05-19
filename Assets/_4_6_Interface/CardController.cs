6cc83186a5807faeafd7b2caed9d24dd65117d8e
jackblack0094 <syomzn10@gmail.com>
2021-05-19 13:22:54 +0900

    tagの名前を変更DeckControllerに追記

--- Assets/_4_6_Interface/CardController.cs
+++ Assets/_4_6_Interface/CardController.cs
@@ -72,16 +72,16 @@ public class CardController : MonoBehaviour, IDragHandler, IPointerDownHandler, 
     }
 
     /// <summary>
-    /// マウスカーソルが現在どのデッキの上にあるかを返す。デッキとは "DeckTag" がタグ付けされた GameObject のこと。
+    /// マウスカーソルが現在どのデッキの上にあるかを返す。デッキとは "ZoneTag" がタグ付けされた GameObject のこと。
     /// なお、デッキは UI オブジェクトつまり Rect Transform コンポーネントがアタッチされたオブジェクトである必要がある。
     /// </summary>
     /// <param name="eventData">PointerEventData 型の引数。マウス操作の情報が入っている。</param>
     /// <returns></returns>
-    GameObject GetCurrentDeck(PointerEventData eventData)
+    public GameObject GetCurrentDeck(PointerEventData eventData)
     {
         var results = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventData, results); // マウスポインタの位置上に重なっている UI を全て results に取得する（※）
-        var result = results.Where(x => x.gameObject.CompareTag("DeckTag")).FirstOrDefault();    // results に入っているオブジェクトのうち、DeckTag が付いているオブジェクトを一つ取得する
+        var result = results.Where(x => x.gameObject.CompareTag("ZoneTag")).FirstOrDefault();    // results に入っているオブジェクトのうち、ZoneTag が付いているオブジェクトを一つ取得する
         return result.gameObject;   // 結果の GameObject を返す
 
         //（※）EventSystem のインターフェイスを使った通常のプログラミングだと、オブジェクトが重なっている場合は「一番上に描画されているオブジェクト」しかマウスの動きを検出できない。
